7345382477d8240e59354c3539f064f2205bfa82
Pablo Aizpiri <pablo.aizpiri@gmail.com>
2019-07-22 22:04:21 -0500

    Add filter parameter

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    field :people, [PersonType], null: false, description: "All people in our database"
+    field :people, [PersonType], null: false, description: "All people in our database" do
+      argument :last_name, String, required: false
+    end
     
-    def people
-      Person.all
+    def people(last_name: nil)
+      people = Person.all
+      people = Person.select { |p| p.last_name == last_name } if last_name
+      people
     end
   end
 end
